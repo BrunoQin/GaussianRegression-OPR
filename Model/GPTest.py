d419534412e89ef99070ad14e7c5163ee31a6546
BrunoQin <brunoqin@163.com>
2018-05-30 20:25:03 +0800

    模型冒烟测试

--- Model/GPTest.py
+++ Model/GPTest.py
@@ -2,33 +2,23 @@ import GPy
 import numpy as np
 import gzip
 import pickle
-import matplotlib; matplotlib.rcParams['figure.figsize'] = (8, 6)
-from matplotlib import pyplot as plt
+# import matplotlib; matplotlib.rcParams['figure.figsize'] = (8, 6)
+# from matplotlib import pyplot as plt
+
 
 with gzip.open('./data/predict.pkl.gz') as fp:
-    predict = pickle.load(fp)
-
-predict = np.array(predict)
-print(predict.shape)
+    predict = np.array(pickle.load(fp))
 
 with gzip.open('./data/start.pkl.gz') as fp:
-    start = pickle.load(fp)
+    start = np.array(pickle.load(fp))
 
-start = np.array(start)
-print(start.shape)
 
-# X = np.random.uniform(0, 6., (20, 1))
-# Y = np.log(X) + np.random.randn(20, 1)*0.05
-#
-# kernel = GPy.kern.RBF(input_dim=1, variance=1., lengthscale=1.)
-# m = GPy.models.GPRegression(X, Y, kernel)
-#
-# fig = m.plot()
-# GPy.plotting.show(fig, filename='basic_gp_regression_notebook')
-#
-# m.optimize(messages=True)
-#
-# fig = m.plot()
-# GPy.plotting.show(fig, filename='basic_gp_regression_notebook_optimized')
-#
-# plt.show()
+K = GPy.kern.Matern32(1)
+icm = GPy.util.multioutput.ICM(input_dim=1, num_outputs=predict.shape[1], kernel=K)
+
+m = GPy.models.GPCoregionalizedRegression(start, predict, kernel=icm)
+m['.*Mat32.var'].constrain_fixed(1.)
+m.optimize()
+print(m)
+
+m.optimize(messages=True)
