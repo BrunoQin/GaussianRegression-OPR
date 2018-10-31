1b15df3632a5361f40f64eddf4023c2f7615dfe1
dl <kris.dacpc@gmail.com>
2018-10-31 19:23:53 +0800

    refactor

--- PrepareData/PrepareTest.py
+++ PrepareData/PrepareTest.py
@@ -7,7 +7,7 @@ data = []
 for i in range(400):
     for j in range(12):
         try:
-            filename = "/Users/Bruno/Desktop/data/" + str(i + 51) + "." + str(j + 1) + ".csv"
+            filename = "/home/dl/Desktop/QinBo/data/" + str(i + 51) + "." + str(j + 1) + ".csv"
             df = pd.read_csv(filename, header=None, dtype=np.float64).values
             df = df.flatten()
             data.append(df)
@@ -16,5 +16,5 @@ for i in range(400):
 
 data = np.array(data)
 print(data.shape)
-with gzip.open('/Users/Bruno/Desktop/data.pkl.gz', 'wb') as f:
+with gzip.open('/home/dl/Desktop/QinBo/data.pkl.gz', 'wb') as f:
     f.write(pickle.dumps(data))
