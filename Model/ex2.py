3736463cb035006a40e3d8e71c3fdc0d8990ea3b
Bruno Qin <brunoqin@163.com>
2018-11-08 21:31:28 +0800

    version 0.1 failed

---
+++ Model/ex2.py
@@ -0,0 +1,92 @@
+import numpy as np
+import tensorflow as tf
+import gpflow
+import gpflow.training.monitor as mon
+from gpflow.test_util import notebook_niter
+
+import myKernel.convkernels as ckern
+from myKernel.CustomTensorBoardTask import CustomTensorBoardTask
+from myKernel.NN import NN
+from myKernel.LoadData import LoadData
+
+float_type = gpflow.settings.float_type
+
+ITERATIONS = notebook_niter(1000)
+
+class Ocean:
+    input_dim = 150 * 160
+    out_dim = 1
+    X, Y, Xt, Yt = LoadData.load_ocean()
+
+
+def ex2():
+    minibatch_size = notebook_niter(1000, test_n=10)
+    gp_dim = 1
+    M = notebook_niter(240, test_n=5)
+
+    ## placeholders
+    X = tf.placeholder(tf.float32, [minibatch_size, Ocean.input_dim])  # fixed shape so num_data works in SVGP
+    Y = tf.placeholder(tf.float32, [minibatch_size, 1])
+    Xtest = tf.placeholder(tf.float32, [None, Ocean.input_dim])
+
+    ## build graph
+
+    with tf.variable_scope('cnn'):
+        f_X = tf.cast(NN.cnn_fn(X, gp_dim), dtype=float_type)
+
+    with tf.variable_scope('cnn', reuse=True):
+        f_Xtest = tf.cast(NN.cnn_fn(Xtest, gp_dim), dtype=float_type)
+
+    k = ckern.WeightedConv(gpflow.kernels.RBF(9), [15, 16], [3, 3]) + gpflow.kernels.White(1, 1e-3)
+
+    # Z = (k.kern_list[0].init_inducing(f_X, M, method="patches-unique")
+    #      if type(k) is gpflow.kernels.Add else
+    #      k.init_inducing(f_X, M, method="patches-unique"))
+
+    # k.trainable = False
+    gp_model = gpflow.models.SVGP(f_X, tf.cast(Y, dtype=float_type),
+                                  k, gpflow.likelihoods.Gaussian(),
+                                  Z=np.zeros((M, gp_dim)), # we'll set this later
+                                  num_latent=1)
+
+    loss = -gp_model.likelihood_tensor
+
+    m, v = gp_model._build_predict(f_Xtest)
+    my, yv = gp_model.likelihood.predict_mean_and_var(m, v)
+
+    with tf.variable_scope('adam'):
+        opt_step = tf.train.AdamOptimizer(0.001).minimize(loss)
+
+    tf_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='adam')
+    tf_vars += tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='cnn')
+
+    ## initialize
+    sess = tf.Session()
+    sess.run(tf.variables_initializer(var_list=tf_vars))
+    gp_model.initialize(session=sess)
+
+    ## reset inducing (they live in a different space as X, so need to be careful with this)
+    ind = np.random.choice(Ocean.X.shape[0], minibatch_size, replace=False)
+
+    fZ = sess.run(f_X, feed_dict={X:Ocean.X[ind]})
+    # Z_0 = kmeans2(fZ, M)[0] might fail
+    Z_0 = fZ[np.random.choice(len(fZ), M, replace=False)]
+
+    def set_gp_param(param, value):
+        sess.run(tf.assign(param.unconstrained_tensor, param.transform.backward(value)))
+
+    set_gp_param(gp_model.feature.Z, Z_0)
+
+    ## train
+    for i in range(ITERATIONS):
+        ind = np.random.choice(Ocean.X.shape[0], minibatch_size, replace=False)
+        sess.run(opt_step, feed_dict={X:Ocean.X[ind], Y:Ocean.Y[ind]})
+
+    ## predict
+    rmse = np.mean((sess.run(my, feed_dict={Xtest:Ocean.Xt}) - Ocean.Yt.shape) ** 2) * 0.5
+    rmse = np.average(rmse.astype(float)) * 100.
+    print('rmse is {:.4f}'.format(rmse))
+
+
+gpflow.reset_default_graph_and_session()
+ex2()
--- Model/myKernel/NN.py
+++ Model/myKernel/NN.py
@@ -10,28 +10,28 @@ class NN():
         """
         conv1 = tf.layers.conv2d(
             inputs=tf.reshape(x, [-1, 150, 160, 1]),
-            filters=32,
+            filters=1,
             kernel_size=[3, 3],
             padding="same",
             activation=tf.nn.relu)
 
-        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[5, 5], strides=5)
 
         conv2 = tf.layers.conv2d(
             inputs=pool1,
-            filters=64,
+            filters=1,
             kernel_size=[3, 3],
             padding="same",
             activation=tf.nn.relu)
 
-        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[5, 5], strides=5)
+        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
         conv3 = tf.layers.conv2d(
             inputs=pool2,
-            filters=128,
+            filters=1,
             kernel_size=[3, 3],
             padding="same",
             activation=tf.nn.relu)
 
-        pool3_flat = tf.reshape(conv3, [-1, 15 * 16 * 128])
-        return tf.layers.dense(inputs=pool3_flat, units=output_dim, activation=tf.nn.relu)
+        pool3_flat = tf.reshape(conv3, [-1, 15 * 16 * 1])
+        return pool3_flat # tf.layers.dense(inputs=pool3_flat, units=output_dim, activation=tf.nn.relu)
